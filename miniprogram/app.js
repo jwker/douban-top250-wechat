ec05a592d69d3abcd99fd60b3c64f23afe039cfe
jwker <jiangweiku@126.com>
2026-03-24 14:18:53 +0800

    1

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -4,7 +4,7 @@ App({
       console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
       wx.cloud.init({
-        env: 'douban-top250-xxxxx', // 替换为你的云环境ID
+        env: 'cloud1-1gd5koldaeae55fc', // 替换为你的云环境ID
         traceUser: true,
       });
     }
